89a7314281b0cd3553b60f2a3e9788c20fb9007d
MuriloSBarbosa <murilo.barbosa@sptech.school>
2024-08-20 12:46:22 -0300

    adding bean name information

--- java-modular-architecture/src/main/java/com/murilobarbosa/java/modular/architecture/JavaModularArchitectureApplication.java
+++ java-modular-architecture/src/main/java/com/murilobarbosa/java/modular/architecture/JavaModularArchitectureApplication.java
@@ -2,8 +2,12 @@ package com.murilobarbosa.java.modular.architecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @SpringBootApplication
+// If there are two classes with the same name in different packages, the FullyQualifiedAnnotationBeanNameGenerator class will generate a unique bean name for each class.
+//@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 public class JavaModularArchitectureApplication {
 
     public static void main(String[] args) {
